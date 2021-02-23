1692c834b3da2fd6bc996660089e22da6c685575
Likh-Alex <likhachov991@protonmail.com>
2021-02-23 21:26:17 +0200

    - extracted adding `partitions` into `List` into `addPartitionsToThread` method

--- src/main/java/processing/SumListWithCallable.java
+++ src/main/java/processing/SumListWithCallable.java
@@ -20,9 +20,7 @@ public class SumListWithCallable {
         List<List<Integer>> partitions = ListUtils.partition(list,list.size() / THREAD_COUNT);
         ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
         List<CallableThread> callableThreads = new ArrayList<>();
-        for (List<Integer> subList : partitions) {
-            callableThreads.add(new CallableThread(subList));
-        }
+        addPartitionsToThread(partitions, callableThreads);
         try {
             executorService.invokeAll(callableThreads);
         } catch (InterruptedException e) {
@@ -31,6 +29,13 @@ public class SumListWithCallable {
         int summary = getThreadsResult(executorService, callableThreads);
         executorService.shutdown();
         return summary;
+    }
+
+    private void addPartitionsToThread(List<List<Integer>> partitions,
+                                       List<CallableThread> callableThreads) {
+        for (List<Integer> partition : partitions) {
+            callableThreads.add(new CallableThread(partition));
+        }
     }
 
     private int getThreadsResult(ExecutorService executorService,
